8ef457f8b270df448436cd35896abe96c2d8f7c4
blukitas <lucasbertolini@otmail.com>
2021-11-27 15:39:32 -0300

    fix typo

--- src/05.FeatureEngineering/745.dataset.baja.r
+++ src/05.FeatureEngineering/745.dataset.baja.r
@@ -30,7 +30,7 @@ fwrite( dataset[  clase_ternaria =="BAJA+1"  & foto_mes>=202001  & foto_mes<=202
 cat("Guardo baja+2")
 #Grabo el dataset
 fwrite( dataset[  clase_ternaria =="BAJA+2"  & foto_mes>=202001  & foto_mes<=202011, ],
-        paste0( "./datasets/dataset_2021_baja_1.csv.gz" ),
+        paste0( "./datasets/dataset_2021_baja_2.csv.gz" ),
         logical01 = TRUE,
         sep= "," )
 
