5e4c6805e53727a03918be682bb7b9f37498300a
blukitas <lucasbertolini@otmail.com>
2021-11-29 20:28:48 -0300

    nov 952

--- src/05.FeatureEngineering/746.dataset.nov.r
+++ src/05.FeatureEngineering/746.dataset.nov.r
@@ -17,12 +17,12 @@ directory.root  <-  "~/buckets/b1/"  #Google Cloud
 setwd( directory.root )
 
 #cargo el dataset donde voy a entrenar
-dataset  <- fread("./datasets/dataset_epic_v1054.csv.gz")
+dataset  <- fread("./datasets/dataset_epic_v952.csv.gz")
 
 cat("Guardo nov")
 #Grabo el dataset
 fwrite( dataset[  foto_mes>=202011  & foto_mes<=202011, ],
-        paste0( "./datasets/dataset_noviembre_1054.gz" ),
+        paste0( "./datasets/dataset_noviembre_952.gz" ),
         logical01 = TRUE,
         sep= "," )
         
