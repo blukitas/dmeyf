a4616154360327281cfd5d8cfa7fb18d8e86345b
blukitas <lucasbertolini@otmail.com>
2021-11-28 21:22:30 -0300

    Extraer noviembre con muchas variables

---
+++ src/05.FeatureEngineering/746.dataset.nov.r
@@ -0,0 +1,31 @@
+
+
+#limpio la memoria
+rm( list=ls() )  #remove all objects
+gc()             #garbage collection
+
+require("data.table")
+require("Rcpp")
+require("rlist")
+require("yaml")
+
+require("lightgbm")
+ 
+
+#defino la carpeta donde trabajo
+directory.root  <-  "~/buckets/b1/"  #Google Cloud
+setwd( directory.root )
+
+#cargo el dataset donde voy a entrenar
+dataset  <- fread("./datasets/dataset_epic_v1054.csv.gz")
+
+cat("Guardo nov")
+#Grabo el dataset
+fwrite( dataset[  foto_mes>=202011  & foto_mes<=202011, ],
+        paste0( "./datasets/dataset_noviembre_1054.gz" ),
+        logical01 = TRUE,
+        sep= "," )
+        
+gc()
+
+quit()
