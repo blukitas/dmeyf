d3da99e224f904b3c8fefe9d1b5fce49a862a283
blukitas <lucasbertolini@otmail.com>
2021-12-06 16:48:14 -0300

    test de hibridacion

--- src/13.hibridacion/1511_hibridacion_de_semillerios_original.r
+++ src/13.hibridacion/1511_hibridacion_de_semillerios_original.r
@@ -8,23 +8,16 @@ gc()             #garbage collection
 require("data.table")
 require("yaml")
 
-# setwd( "~/buckets/b1/" )
-setwd( "/home/lucas/Desktop/2021/Maestria/02.05.Data.Mining.E.y.F/Repo.TP/dmeyf/src/99.buckets/" )
+setwd( "~/buckets/b1/" )
 
 #notar que son de experimentos distintos, y uno sumo 200 semillas y el otro apenas 70
-# E1000 - s1511
-# arch1  <- "./kaggle/E5058/Esemillerio_130_14000.csv"
-# arch2  <- "./kaggle/E5032/semillerio_150_15000.csv"
-# arch3  <- "./kaggle/E5041/Esemillerio_130_15000.csv"
-# E1001 - s1512
-arch1  <- "./kaggle/E5058/Esemillerio_130_11000.csv"
-arch2  <- "./kaggle/E5032/semillerio_150_11000.csv"
-arch3  <- "./kaggle/E5041/Esemillerio_130_11000.csv"
+arch1  <- "./kaggle/E1427/meseta/E1427_s1438_200_14000.csv"
+arch2  <- "./kaggle/E1421/meseta/E1421_s1431_70_14000.csv"
 
 
 kexperimento <-  NA
 
-kscript <-  "s1512"
+kscript <-  "s1511"
 
 #------------------------------------------------------------------------------
 #Funcion que lleva el registro de los experimentos
@@ -57,26 +50,23 @@ kkaggle       <- paste0("./kaggle/E",kexperimento, "/E",  kexperimento, "_", ksc
 #leo lo datasets
 modelo1  <- fread( arch1 )
 modelo2  <- fread( arch2 )
-modelo3  <- fread( arch3 )
 
 #esta hermosamente ordenados por probabilidad descente
 modelo1[ , ranking := .I ]
 modelo2[ , ranking := .I ]
-modelo3[ , ranking := .I ]
 
 
 #los ordeno antes de aparearlos
 #como tienen exactamente los mismos numero de cliente, todo va a ir bien
 setorder( modelo1, numero_de_cliente )
 setorder( modelo2, numero_de_cliente )
-setorder( modelo3, numero_de_cliente )
 
 modelo_nuevo  <- copy(  modelo1[  , c("numero_de_cliente"), with=FALSE ] )
-modelo_nuevo[ , ranking  :=  130*modelo1$ranking + 150*modelo2$ranking + 130*modelo3$ranking ]   #ATENCION  el 200 es por las 200 semillas del modelo1, y el 70 por las 70 del modelo2
+modelo_nuevo[ , ranking  :=  200*modelo1$ranking + 70*modelo2$ranking ]   #ATENCION  el 200 es por las 200 semillas del modelo1, y el 70 por las 70 del modelo2
 
 setorder( modelo_nuevo, ranking ) 
 
-for(  corte  in seq( 10000, 14000, 250) ) #imprimo cortes en 10000, 11000, 12000, 13000, 14000 y 15000
+for(  corte  in seq( 11000, 14000, 1000) ) #imprimo cortes en 10000, 11000, 12000, 13000, 14000 y 15000
 {
   modelo_nuevo[ ,  Predicted := 0L ]
   modelo_nuevo[ 1:corte,  Predicted := 1L ]  #me quedo con los primeros
